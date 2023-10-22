88df92e5ef7419b5bc9c3cf0d781537a5063d4a1
Kenny Strawn <Kenny.Strawn@gmail.com>
2023-10-22 16:09:59 -0700

    rustfmt

--- bios/stage-4/src/main.rs
+++ bios/stage-4/src/main.rs
@@ -129,20 +129,12 @@ pub extern "C" fn _start(info: &mut BiosInfo) -> ! {
     };
 
     #[allow(deprecated)]
-    if config
-        .frame_buffer
-        .minimum_framebuffer_height
-        .is_none()
-    {
+    if config.frame_buffer.minimum_framebuffer_height.is_none() {
         config.frame_buffer.minimum_framebuffer_height =
             kernel.config.frame_buffer.minimum_framebuffer_height;
     }
     #[allow(deprecated)]
-    if config
-        .frame_buffer
-        .minimum_framebuffer_width
-        .is_none()
-    {
+    if config.frame_buffer.minimum_framebuffer_width.is_none() {
         config.frame_buffer.minimum_framebuffer_width =
             kernel.config.frame_buffer.minimum_framebuffer_width;
     }
